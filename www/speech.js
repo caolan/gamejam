b9c3434990598d71f973de271ff1684cd1854312
Caolan McMahon <caolan@caolanmcmahon.com>
2013-10-13 13:26:29 +0100

    turn off speech recognition on final result

--- www/speech.js
+++ www/speech.js
@@ -45,6 +45,7 @@ speech.listen = function (callback) {
     speech.recognition.onresult = function (event) {
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
+                speech.recognition.stop();
                 var str = event.results[i][0].transcript.toLowerCase();
                 str = str.replace(/^\s+|\s+$/,''); //trim
                 callback(null, str);
